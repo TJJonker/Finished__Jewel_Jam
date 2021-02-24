55d33953cc73aae1a82f17f3b8da6f706b27c6e7
Tom <60851185+TJJonker@users.noreply.github.com>
2021-02-24 17:54:51 +0100

    Added conversion from Screen position to GameWorld position and fixed a minor bug.

--- Jewel_Jam/JewelJam.cs
+++ Jewel_Jam/JewelJam.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Text.Encodings.Web;
 
 namespace Jewel_Jam
 {
@@ -10,7 +11,7 @@ namespace Jewel_Jam
         private SpriteBatch spriteBatch;
         private Point worldSize, windowSize;
         private InputHelper inputHelper;
-        private Texture2D background;
+        private Texture2D background, cursorSprite;
         private Matrix spriteScale;
 
         private bool FullScreen
@@ -31,6 +32,7 @@ namespace Jewel_Jam
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             background = Content.Load<Texture2D>("spr_background");
+            cursorSprite = Content.Load<Texture2D>("spr_double");
 
             worldSize = new Point(background.Width, background.Height);
             windowSize = new Point(1024, 768);
@@ -52,54 +54,93 @@ namespace Jewel_Jam
             GraphicsDevice.Clear(Color.Black);
             spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, spriteScale);
             spriteBatch.Draw(background, Vector2.Zero, Color.White);
+            spriteBatch.Draw(cursorSprite, ScreenToWorld(inputHelper.MousePosition), Color.White);
             spriteBatch.End();
         }
 
+
+        /// <summary>
+        /// Changes the resolution to full screen or windowed mode
+        /// </summary>
+        /// <param name="fullScreen">Full screen or not</param>
         private void ApplyResolutionSettings(bool fullScreen)
         {
+            // Sets screen to fullscreen or not
             graphics.IsFullScreen = fullScreen;
 
             Point screenSize;
             if (fullScreen)
             {
+                // sets screen size to fullscreen
                 screenSize = new Point(
                     GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width,
                     GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height);
             }
             else
             {
+                // sets screen size to window size
                 screenSize = windowSize;
             }
 
+            // Changes screen size 
             graphics.PreferredBackBufferWidth = screenSize.X;
             graphics.PreferredBackBufferHeight = screenSize.Y;
             graphics.ApplyChanges();
+
+            // Calculates Viewport AspectRatio
             GraphicsDevice.Viewport = CalculateViewport(screenSize);
+
+            // Sets scale for the game
             spriteScale = Matrix.CreateScale(
                 (float)GraphicsDevice.Viewport.Width / worldSize.X,
                 (float)GraphicsDevice.Viewport.Height / worldSize.Y, 1);
         }
 
+        /// <summary>
+        /// Calculates and returns viewport with the right aspect-ratio
+        /// </summary>
+        /// <param name="windowSize"> Window size chosen for the game </param>
+        /// <returns></returns>
         private Viewport CalculateViewport(Point windowSize)
         {
             Viewport viewport = new Viewport();
-            float gameAspectRatio = (float)worldSize.X / worldSize.X;
+            float gameAspectRatio = (float)worldSize.X / worldSize.Y;
             float windowAspectRatio = (float)windowSize.X / windowSize.Y;
+            // Checks aspectRatios of window and GameWorld
             if (windowAspectRatio > gameAspectRatio)
             {
+                // Window too wide
                 viewport.Width = (int)(windowSize.Y * gameAspectRatio);
                 viewport.Height = windowSize.Y;
             }
             else
             {
+                // Window too high
                 viewport.Width = windowSize.X;
-                viewport.Height = (int)(windowAspectRatio / gameAspectRatio);
+                viewport.Height = (int)(windowSize.X / gameAspectRatio);
             }
 
             viewport.X = (windowSize.X - viewport.Width) / 2;
             viewport.Y = (windowSize.Y - viewport.Height) / 2;
 
             return viewport;
+        }
+
+        /// <summary>
+        /// Convert Screen position to GameWorld position
+        /// </summary>
+        /// <param name="screenPosition"> Position on screen </param>
+        /// <returns></returns>
+        private Vector2 ScreenToWorld(Vector2 screenPosition)
+        {
+            // Takes top left position of the game window
+            Vector2 viewportTopLeft = new Vector2(GraphicsDevice.Viewport.X, GraphicsDevice.Viewport.Y);
+
+            // Calculates scale
+            float screenToWorldScale = worldSize.X / (float)GraphicsDevice.Viewport.Width;
+
+            // Returns position in GameWorld
+            return (screenPosition - viewportTopLeft) * screenToWorldScale;
         }
     }
 }
