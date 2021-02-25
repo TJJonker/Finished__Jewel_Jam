a3dea28d86fee8a705e5074152f9be25e2b2e173
Tom <60851185+TJJonker@users.noreply.github.com>
2021-02-25 22:35:02 +0100

    Made a superclass ExtendedGame with all the basic stuff from JewelJam the game needs to initialize

--- Jewel_Jam/JewelJam.cs
+++ Jewel_Jam/JewelJam.cs
@@ -24,11 +24,7 @@ namespace Jewel_Jam
         private int[,] grid;
         private static Random random;
 
-        private bool FullScreen
-        {
-            get { return graphics.IsFullScreen; }
-            set { ApplyResolutionSettings(value); }
-        }
+
 
         public JewelJam()
         {
@@ -88,89 +84,7 @@ namespace Jewel_Jam
             spriteBatch.End();
         }
 
-        /// <summary>
-        /// Changes the resolution to full screen or windowed mode
-        /// </summary>
-        /// <param name="fullScreen">Full screen or not</param>
-        private void ApplyResolutionSettings(bool fullScreen)
-        {
-            // Sets screen to fullscreen or not
-            graphics.IsFullScreen = fullScreen;
-
-            Point screenSize;
-            if (fullScreen)
-            {
-                // sets screen size to fullscreen
-                screenSize = new Point(
-                    GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width,
-                    GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height);
-            }
-            else
-            {
-                // sets screen size to window size
-                screenSize = windowSize;
-            }
-
-            // Changes screen size
-            graphics.PreferredBackBufferWidth = screenSize.X;
-            graphics.PreferredBackBufferHeight = screenSize.Y;
-            graphics.ApplyChanges();
-
-            // Calculates Viewport AspectRatio
-            GraphicsDevice.Viewport = CalculateViewport(screenSize);
-
-            // Sets scale for the game
-            spriteScale = Matrix.CreateScale(
-                (float)GraphicsDevice.Viewport.Width / worldSize.X,
-                (float)GraphicsDevice.Viewport.Height / worldSize.Y, 1);
-        }
-
-        /// <summary>
-        /// Calculates and returns viewport with the right aspect-ratio
-        /// </summary>
-        /// <param name="windowSize"> Window size chosen for the game </param>
-        /// <returns></returns>
-        private Viewport CalculateViewport(Point windowSize)
-        {
-            Viewport viewport = new Viewport();
-            float gameAspectRatio = (float)worldSize.X / worldSize.Y;
-            float windowAspectRatio = (float)windowSize.X / windowSize.Y;
-            // Checks aspectRatios of window and GameWorld
-            if (windowAspectRatio > gameAspectRatio)
-            {
-                // Window too wide
-                viewport.Width = (int)(windowSize.Y * gameAspectRatio);
-                viewport.Height = windowSize.Y;
-            }
-            else
-            {
-                // Window too high
-                viewport.Width = windowSize.X;
-                viewport.Height = (int)(windowSize.X / gameAspectRatio);
-            }
-
-            viewport.X = (windowSize.X - viewport.Width) / 2;
-            viewport.Y = (windowSize.Y - viewport.Height) / 2;
-
-            return viewport;
-        }
-
-        /// <summary>
-        /// Convert Screen position to GameWorld position
-        /// </summary>
-        /// <param name="screenPosition"> Position on screen </param>
-        /// <returns></returns>
-        private Vector2 ScreenToWorld(Vector2 screenPosition)
-        {
-            // Takes top left position of the game window
-            Vector2 viewportTopLeft = new Vector2(GraphicsDevice.Viewport.X, GraphicsDevice.Viewport.Y);
-
-            // Calculates scale
-            float screenToWorldScale = worldSize.X / (float)GraphicsDevice.Viewport.Width;
-
-            // Returns position in GameWorld
-            return (screenPosition - viewportTopLeft) * screenToWorldScale;
-        }
+        
 
         /// <summary>
         /// Deletes the bottom row of the grid and spawns a new row on top
