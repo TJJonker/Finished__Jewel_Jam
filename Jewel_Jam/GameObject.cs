4f210224b35dfaffbe7945e9a94b83b150fa4696
Tom <60851185+TJJonker@users.noreply.github.com>
2021-02-25 22:57:17 +0100

    Created a GameObject

---
+++ Jewel_Jam/GameObject.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jewel_Jam
+{
+    class GameObject
+    {
+        public Vector2 Position { get; set; }
+        protected Vector2 velocity;
+
+        public bool Visible { get; set; }
+
+        public GameObject()
+        {
+            Position = Vector2.Zero;
+            velocity = Vector2.Zero;
+            Visible = true;
+        }
+
+        public virtual void HandleInput(InputHelper inputHelper) { }
+        public virtual void Update(GameTime gameTime)
+        {
+            Position += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+        public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch) { }
+        public virtual void Reset()
+        {
+            velocity = Vector2.Zero;
+        }
+    }
+}
