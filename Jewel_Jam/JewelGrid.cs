7548b07b75a4360444349d6d78b65059990e28dd
Tom <60851185+TJJonker@users.noreply.github.com>
2021-02-26 22:29:25 +0100

    Moved all JewelGrid related methods and variables from JewelJam to JewelGrid to clean up JewelGrid

---
+++ Jewel_Jam/JewelGrid.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Jewel_Jam
+{
+    internal class JewelGrid : GameObject
+    {
+        private Jewel[,] grid;
+        private int gridWidth, gridHeight, cellSize;
+
+        public JewelGrid(int width, int height, int cellSize, Vector2 offset)
+        {
+            gridWidth = width;
+            gridHeight = height;
+            this.cellSize = cellSize;
+            Position = offset;
+
+            Reset();
+        }
+
+        public override void Reset()
+        {
+            grid = new Jewel[gridWidth, gridHeight];
+
+            for (int x = 0; x < gridWidth; x++)
+                for (int y = 0; y < gridHeight; y++)
+                {
+                    grid[x, y] = new Jewel(ExtendedGame.Random.Next(3));
+                    grid[x, y].Position = GetCellPosition(x, y);
+                }
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            foreach (Jewel jewel in grid)
+                jewel.Draw(gameTime, spriteBatch);
+        }
+
+        /// <summary>
+        /// Deletes the bottom row of the grid and spawns a new row on top
+        /// </summary>
+        private void MoveRowsDown()
+        {
+            // Replaces all the Jewels with the Jewel above them
+            for (int y = gridHeight - 1; y > 0; y--)
+                for (int x = 0; x < gridWidth; x++)
+                {
+                    grid[x, y] = grid[x, y - 1];
+                    grid[x, y].Position = GetCellPosition(x, y);
+                }
+
+            // Fills top row with new Jewels
+            for (int x = 0; x < gridWidth; x++)
+            {
+                grid[x, 0] = new Jewel(ExtendedGame.Random.Next(3));
+                grid[x, 0].Position = GetCellPosition(x, 0);
+            }
+        }
+
+        private Vector2 GetCellPosition(int x, int y)
+        {
+            return Position + new Vector2(x * cellSize, y * cellSize);
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            if (inputHelper.KeyPressed(Keys.Space))
+                MoveRowsDown();
+        }
+    }
+}
--- Jewel_Jam/JewelJam.cs
+++ Jewel_Jam/JewelJam.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using System;
 
 namespace Jewel_Jam
 {
@@ -10,21 +7,13 @@ namespace Jewel_Jam
         private const int GridWidth = 5;
         private const int GridHeight = 10;
         private const int CellSize = 85;
+        private Vector2 GridOffset = new Vector2(85, 150);
 
-        // Vector2 is not allowed to be a constant
-        private readonly Vector2 GridOffset = new Vector2(85, 150);
-
-        private Texture2D[] jewels;
-        private int[,] grid;
+        private JewelGrid jewelGrid;
 
         public JewelJam()
         {
             IsMouseVisible = true;
-            grid = new int[GridWidth, GridHeight];
-
-            for (int x = 0; x < GridWidth; x++)
-                for (int y = 0; y < GridHeight; y++)
-                    grid[x, y] = Random.Next(3);
         }
 
         protected override void LoadContent()
@@ -34,54 +23,12 @@ namespace Jewel_Jam
             SpriteGameObject background = new SpriteGameObject("spr_background");
             gameWorld.Add(background);
 
-            jewels = new Texture2D[3];
-            jewels[0] = Content.Load<Texture2D>("spr_single_jewel1");
-            jewels[1] = Content.Load<Texture2D>("spr_single_jewel2");
-            jewels[2] = Content.Load<Texture2D>("spr_single_jewel3");
+            jewelGrid = new JewelGrid(GridWidth, GridHeight, CellSize, GridOffset);
+            gameWorld.Add(jewelGrid);
 
             worldSize = new Point(background.Width, background.Height);
 
             FullScreen = false;
-        }
-
-        protected override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
-
-            if (inputHelper.KeyPressed(Keys.Space))
-                MoveRowsDown();
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.Black);
-            spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, spriteScale);
-            spriteBatch.Draw(background, Vector2.Zero, Color.White);
-
-            // Drawing the grid
-            for (int x = 0; x < GridWidth; x++)
-                for (int y = 0; y < GridHeight; y++)
-                {
-                    Vector2 position = GridOffset + new Vector2(x, y) * CellSize;
-                    spriteBatch.Draw(jewels[grid[x, y]], position, Color.White);
-                }
-
-            spriteBatch.End();
-        }
-
-        /// <summary>
-        /// Deletes the bottom row of the grid and spawns a new row on top
-        /// </summary>
-        private void MoveRowsDown()
-        {
-            // Replaces all the Jewels with the Jewel above them
-            for (int y = GridHeight - 1; y > 0; y--)
-                for (int x = 0; x < GridWidth; x++)
-                    grid[x, y] = grid[x, y - 1];
-
-            // Fills top row with new Jewels
-            for (int x = 0; x < GridWidth; x++)
-                grid[x, 0] = Random.Next(3);
         }
     }
 }
