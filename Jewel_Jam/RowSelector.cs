26fbdbfc6f38dab38fb5866b45f829861ad02e6b
Tom <60851185+TJJonker@users.noreply.github.com>
2021-02-27 14:17:54 +0100

    Created a RowSelector class and added the designated sprite

---
+++ Jewel_Jam/RowSelector.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Jewel_Jam
+{
+    internal class RowSelector : SpriteGameObject
+    {
+        private int selectedRow;
+        private JewelGrid grid;
+
+        public RowSelector(JewelGrid grid) : base("spr_selector_frame")
+        {
+            this.grid = grid;
+            selectedRow = 0;
+            origin = new Vector2(10, 10);
+        }
+    }
+}
